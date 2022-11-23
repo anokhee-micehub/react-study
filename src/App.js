188f441ec9335e032499500e7601f6ef05f9c07d
anokhee <aoh1223@micehub.com>
2022-11-23 16:15:43 +0900

    🎉 22-11-23 study_1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HellowWorld = () => <h1>Hello World</h1>;
+
+const App = () => (
+    <div>
+        App
+        <HellowWorld />
+    </div>
+);
+
+export default App
