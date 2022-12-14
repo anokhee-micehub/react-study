d1b8002dc41c3b3a55d8e9987db3a71fc9f4dedb
okhee <aoh1223@micehub.com>
2022-12-14 13:40:59 +0900

    ✅ 22-12-07 study_3

--- src/App.js
+++ src/App.js
@@ -16,15 +16,30 @@ const orangeTheme = {
   },
 };
 
+const propsObj = {
+  image: (
+    <img
+      src="https://googlefonts.github.io/korean/public/assets/og-img.png"
+      alt="Google Fonts + 한국어"
+    />
+  ),
+  title: "Google Fonts + 한국어",
+  category: "OSS",
+  summary: `좋은 타이포그래피를 통해 웹은 더욱 아름답고, 빠르며, 누구나 참여할 수 있는 공간이 될 수
+  있습니다. Google Fonts는 이 사이트에 수록된 오픈 소스 한글 폰트를 머신 러닝에 기반을 둔
+  최적화 기술을 통해 시범적으로 제공합니다.`,
+  link: "https://googlefonts.github.io/korean/",
+};
+
 function App() {
   return (
     <div>
       <MaterialCard />
       <ThemeProvider theme={blueTheme}>
-        <StyledCard />
+        <StyledCard {...propsObj} />
       </ThemeProvider>
       <ThemeProvider theme={orangeTheme}>
-        <StyledCard />
+        <StyledCard {...propsObj} />
       </ThemeProvider>
     </div>
   );
