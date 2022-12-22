df243dfef72102c19277375e78a38a1205581b5a
anokhee <aoh1223@micehub.com>
2022-12-22 16:04:12 +0900

    ✅ 22-12-22 study_5

--- src/App.js
+++ src/App.js
@@ -1,92 +1,115 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import { Chip, chipClasses } from "@mui/material";
+import {
+  Box,
+  Button,
+  capitalize,
+  Chip,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
 import MaterialCard from "./components/MaterialCard";
-import MuiBoxList from "./components/MuiBoxList";
-// import CssModuleCard from "./components/CssModuleCard";
-import StyledCard from "./components/StyledCard";
+import { alpha, Stack } from "@mui/system";
 
-const blueTheme = {
-  colors: {
-    primary: "#228be6",
+const muiTheme = createTheme({
+  palette: {
+    primary: {
+      light: "#F27289",
+      main: "#F23D3D",
+      dark: "#8C2323",
+      contrastText: "#FFFFFF",
+    },
+    brand: {
+      light: "#0041E6",
+      main: "#00237F",
+      dark: "#00071A",
+      contrastText: "#FFFFFF",
+    },
+  },
+  typography: {
+    h6: {
+      fontFamily: ["Jua", "sans-serif"].join(","),
+      padding: "0.5rem 1rem",
+      borderRadius: "0.25rem",
+    },
+  },
+  spacing: 8,
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
+  components: {
+    MuiChip: {
+      defaultProps: {
+        variant: "outlined",
+        color: "primary",
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState, theme }) => ({
+          transition: theme.transitions.create(["all"]),
+          "&:hover": {
+            transform: `translateY(${theme.spacing(-0.5)})`,
+            backgroundColor: theme.palette[ownerState.color].main,
+            boxShadow: `0 ${theme.spacing(0.5)} 0 0 ${theme.palette[ownerState.color].dark}`,
+            transition: theme.transitions.create(["all"], { duration: 150 }),
+            ...(ownerState.variant === "soft" && {
+              color: "#ffffff",
+            }),
+          },
+          "&:active": {
+            transform: `translateY(0)`,
+            backgroundColor: theme.palette[ownerState.color].light,
+            boxShadow: `0 0 0 0 ${theme.palette[ownerState.color].dark}`,
+          },
+        }),
+      },
+      variants: [
+        {
+          props: { variant: "soft" },
+          style: ({ ownerState, theme }) => ({
+            backgroundColor: alpha(theme.palette[ownerState.color].main, 0.2),
+            color: theme.palette[ownerState.color].dark,
+            fontWeight: theme.typography.fontWeightBold,
+          }),
+        },
+      ],
+    },
+  },
+});
+
+muiTheme.typography.h6 = {
+  ...muiTheme.typography.h6,
+  color: muiTheme.palette.primary.main,
+  [`${muiTheme.breakpoints.up("laptop")}`]: {
+    color: "#FFFFFF",
+    backgroundColor: muiTheme.palette.primary.dark,
+    "&:hover": {
+      backgroundColor: muiTheme.palette.primary.main,
+    },
   },
 };
 
-const orangeTheme = {
-  colors: {
-    primary: "#ef891c",
-  },
-};
-
-const propsObj = {
-  image: (
-    <img
-      src="https://googlefonts.github.io/korean/public/assets/og-img.png"
-      alt="Google Fonts + 한국어"
-    />
-  ),
-  title: "Google Fonts + 한국어",
-  category: "OSS",
-  summary: `좋은 타이포그래피를 통해 웹은 더욱 아름답고, 빠르며, 누구나 참여할 수 있는 공간이 될 수
-  있습니다. Google Fonts는 이 사이트에 수록된 오픈 소스 한글 폰트를 머신 러닝에 기반을 둔
-  최적화 기술을 통해 시범적으로 제공합니다.`,
-  link: "https://googlefonts.github.io/korean/",
-};
-
-function App() {
-  return (
-    <div>
-      <Chip
-        label="oki"
-        variant="outlined"
-        color="success"
-        onDelete={() => {}}
-        sx={{
-          [`& .${chipClasses.label}`]: {
-            fontWeight: "bold",
-          },
-          [`& .${chipClasses.deleteIcon}`]: {
-            color: "error.main",
-            ":hover, :active": {
-              color: "error.dark",
-            },
-          },
-        }}
-      />
-      <MuiBoxList />
+const App = () => (
+  <ThemeProvider theme={muiTheme}>
+    <CssBaseline />
+    <Box p={4}>
       <MaterialCard />
-      <ThemeProvider theme={blueTheme}>
-        <StyledCard {...propsObj} />
-      </ThemeProvider>
-      <ThemeProvider theme={orangeTheme}>
-        <StyledCard {...propsObj} />
-      </ThemeProvider>
-    </div>
-  );
-}
+      <Stack direction="row" gap={2} m={2}>
+        {["primary", "secondary", "error", "warning", "info", "success", "brand"].map(v => (
+          <Button variant="soft" key={v} color={v}>
+            {capitalize(v)}
+          </Button>
+        ))}
+      </Stack>
+      <Chip label="React Router" />
+    </Box>
+  </ThemeProvider>
+);
 
 export default App;
-
-{
-  /* <CssModuleCard /> */
-}
-{
-  /* ordered값을 그냥 적으면 암묵적true */
-}
-{
-  /* <List ordered fontSize="20px">
-  <ListItem>아침</ListItem>
-  <li>점심</li>
-  <li>저녁</li>
-</List> */
-}
-{
-  /* orderd값을 아예 적지않으면 기본값 적용 */
-}
-{
-  /* <List fontSize="1.5rem">
-  <li>사과</li>
-  <li>바나나</li>
-  <li>포도</li>
-</List> */
-}
